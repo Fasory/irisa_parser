7fbbe3f937a30d884b004ffb699e884990195817
Fasory <72564803+Fasory@users.noreply.github.com>
2022-02-18 02:36:16 +0100

    Add structure __init__

---
+++ app/extraction/__init__.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8 -*-
+
+"""
+Ce module a pour objectif d'extraire des données et
+les méta-données associées à partir d'un fichier.
+
+Ce module fait suite au module controller et précède
+le module processing.
+"""
+
+# info
+__version__ = "0.1"
+__author__ = "Clément Bouquet"
+
+from extraction import run
