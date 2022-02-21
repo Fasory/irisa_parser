dd428844e55cc5338bc15e04eaa37ca37677479b
julie <juliea.bidault@gmail.com>
2022-02-21 15:22:17 +0100

    Controller: -Single argument check; -Check if directory exists; -Checking read and write permissionson the directory.

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,8 +1,31 @@
+#!/usr/bin/python
 """
-To do
+Le controleur permet de vérifier les entrées du programme par :
+- La vérification de la syntaxe de la commande ;
+- La vérification de l'existence & des permissions du dossier d'entrée ;
+- La vérification du format & des permissions des fichiers d'entrées.
+
+-- Input : String (commande)
+-- Output : Fichier
 """
 
 OUTPUT_DIR = "/out"
+# Ce module donne accès à tous les arguments de ligne de commande
+import sys
+import os
 
 def run():
-    1+2
+
+def controler():
+    # On vérifie qu'il n'y a qu'un seul et unique argument
+    if len(sys.argv) != 2:
+        print("Usage: irisa_parser.py <pathDirectory>")
+        print(exit)
+        exit()
+    else:
+        pathDirectory=sys.argv[1]
+        # On vérifie si le répertoire/fichier entré existe
+        if os.path.exists(pathDirectory)!=True:
+            sys.exit("error -> <pathDirectory> does not exist")
+        if not os.access(pathDirectory, os.F_OK | os.R_OK | os.W_OK):
+            sys.exit("error -> you do not have write AND read permissions")
