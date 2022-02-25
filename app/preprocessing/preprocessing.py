1cecbcb1cbf792fb307ed1a1522c62818267c0b5
Yoann DEWILDE <yoanndewilde@gmail.com>
2022-02-25 15:27:06 +0100

    Preprocessing: Reorganize all the contents (not finished)

--- app/preprocessing/preprocessing.py
+++ app/preprocessing/preprocessing.py
@@ -5,6 +5,7 @@ This file is the main file of the pre-preocessing module.
 """
 
 from TextPreprocessingResult import TextPreprocessingResult
+import processing
 
 
 def run(extraction_result):
@@ -16,13 +17,27 @@ def run(extraction_result):
         for c in p.contents:
             contents.append(c)
 
-    return TextPreprocessingResult(filename, contents)
+    contents = organize_text(contents)
+
+    processing.run(TextPreprocessingResult(filename, contents))
 
 
 def organize_text(contents):
     new_contents = []
 
     i = 0
-    while i < len(contents):
+    while i < len(contents) - 1:
         c1 = contents[i]
         c2 = contents[i + 1]
+
+        merged = c1
+        while c1.is_near(c2) and i < len(contents) - 1:
+            merged = merged.merge_with(c2)
+
+            i += 1
+            c1 = contents[i]
+            c2 = contents[i + 1]
+
+        i += 1
+
+    return new_contents
