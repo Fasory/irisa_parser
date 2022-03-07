002c047ae35381425ea7d1e54253811b9c29d818
julie <juliea.bidault@gmail.com>
2022-03-07 16:12:34 +0100

    restitution.py : add XML

--- restitution/restitution.py
+++ restitution/restitution.py
@@ -36,8 +36,8 @@ def restitutionText(file, processingResult):
         file.write("Auteurs : ")
         if len(processingResult.authors) > 0:
             file.write(processingResult.authors[0])
-        for author in processingResult.authors[1:]:
-            file.write(", " + author)
+            for author in processingResult.authors[1:]:
+                file.write(", " + author)
         file.write("\n")
 
         file.write("Résumé : ")
@@ -45,11 +45,36 @@ def restitutionText(file, processingResult):
 
 
 def restitutionXML(file, processingResult):
-    addOpenBalise(file, "article")
-    file.write()
+    file.write("<article>\n")
 
-def addOpenBalise(file, baliseName) :
-    file.write("<" + baliseName + ">")
+    file.write("\t<preamble>")
+    file.write("Fichier original : " + processingResult.original_file_name)
+    file.write("</preamble>\n")
 
-def addCloseBalise(file, baliseName):
-    file.write("</" + baliseName + ">")
+    file.write("\t<titre> ")
+    file.write("Titre : " + processingResult.title)
+    file.write("</titre> \n")
+
+    if len(processingResult.authors) > 0:
+        file.write("\t<auteurs>\n")
+        for author in processingResult.authors:
+            file.write("\t\t<auteur>")
+            file.write("\t\t\t<name>")
+            file.write(author.name)
+            file.write("</name>\n")
+            file.write("\t\t\t<mail>")
+            file.write(author.mail)
+            file.write("</mail>\n")
+            file.write("\t\t</auteur>")
+        file.write("</auteurs>\n")
+
+    file.write("\t<abstract>")
+    file.write(processingResult.abstract)
+    file.write("<abstract>\n")
+    file.write("\t<biblio>\n")
+
+    file.write("\t</biblio>\n")
+    file.write("</article>\n")
+
+def addTab(file, nb) :
+    return None
