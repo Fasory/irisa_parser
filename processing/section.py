0b71cdecdeb2baf4aa5a4eb34fd4c053fc1c8186
Yoann DEWILDE <yoanndewilde@gmail.com>
2022-04-23 23:19:23 +0200

    [NOT WORKING] Tried to fix `Section`

--- processing/section.py
+++ processing/section.py
@@ -13,7 +13,8 @@ class Section(Enum):
     REFERENCE = 7
     ANONYMOUS = 8
 
-    def __int__(self):
+    def compute_next(self):
+        #Enum.__init__(self)
         if self == self.ANONYMOUS:
             self._next = []
         elif self == self.BODY:
@@ -36,7 +37,10 @@ class Section(Enum):
                           self.REFERENCE][self.value + 1:]
 
     def create_ananymous(self):
+        self.compute_next()
+
         section = self.ANONYMOUS
+        section.compute_next()
         section._next = self._next
         return section
 
@@ -89,7 +93,7 @@ class Section(Enum):
                     return section, insert_in_buffer
         return None, None
 
-    def math_next(self, content, major_font, major_font_size):
+    def match_next(self, content, major_font, major_font_size):
         target = content.string.strip().lower()
         if self == self.TITLE:
             return False, False
@@ -104,7 +108,7 @@ class Section(Enum):
                     or content.major_font != major_font and content.major_font_size == major_font_size), False
         elif self == self.BODY:
             for other_section in self.BODY._next[1:]:
-                if other_section != self.ANONYMOUS and other_section.math_next(content, major_font, major_font_size):
+                if other_section != self.ANONYMOUS and other_section.match_next(content, major_font, major_font_size):
                     return False, False
             return content.major_font_size > major_font_size or content.major_font != major_font and \
                 content.major_font_size == major_font_size, True
@@ -128,6 +132,8 @@ class Section(Enum):
 
 def section_extraction(result, text_processing_result, first_research, limit_research=None):
     current_section = first_research
+    current_section.compute_next()
+
     buffer_section = ""
     for content in result.contents:
         if current_section is not None and current_section.match_string(content,
